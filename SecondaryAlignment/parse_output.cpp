620eb57e813099d1a60f421d217664e3b3d5bee9
Annak17 <annak17@lakesideschool.org>
2016-07-27 14:55:04 -0700

    Cleaning up.

--- SecondaryAlignment/parse_output.cpp
+++ SecondaryAlignment/parse_output.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -5,12 +6,23 @@
 #include <string>
 #include <vector>
 
-struct attributes {
+int n_second = 0;
+
+class attributes {
+public:
   std::string d_gene;
   int score;
   std::vector<int> ranges;
 };
-struct align_info {
+
+struct sort_by_score {
+  inline bool operator()(const attributes &L, const attributes &R) {
+    return L.score > R.score;
+  }
+};
+
+class align_info {
+public:
   std::string query_name;
   std::vector<attributes> attributes_vec;
 };
@@ -53,14 +65,14 @@ void AddRanges(std::vector<int> &vec, std::string line) {
   vec.push_back(range[1]);
 }
 
-std::string StripLabel(std::string label, std::string name, bool query) {
+std::string StripLabel(std::string label, std::string name, bool target) {
   std::vector<std::string> v;
   ToVector(v, label);
   int i = 0;
   std::string nolabel = "";
   while (true) {
     if (v[i] == name) {
-      if (query)
+      if (target)
         for (int j = i + 1; j < v.size(); j++)
           nolabel += v[j] + " ";
       else
@@ -71,6 +83,29 @@ std::string StripLabel(std::string label, std::string name, bool query) {
   }
   return nolabel;
   return v[1];
+}
+
+void FindSecondary(std::vector<attributes> &vec) {
+
+  std::sort(vec.begin(), vec.end(), sort_by_score());
+  int best = vec[0].score;
+
+  if (vec[1].score >= best - 5) {
+    n_second++;
+    std::cout << "D: " << vec[0].d_gene << "\n";
+    std::cout << "Best: " << best << "\n";
+  }
+
+  for (int i = 1; i < vec.size(); i++) {
+    if (vec[i].score >= best - 5) {
+      std::cout << "Score:" << vec[i].score << "\n";
+      std::cout << "D: " << vec[i].d_gene << "\n";
+      std::cout << "Query range: " << vec[i].ranges[0] << "-"
+                << vec[i].ranges[1] << "\n";
+      std::cout << "Target range: " << vec[i].ranges[2] << "-"
+                << vec[i].ranges[3] << "\n\n";
+    }
+  }
 }
 
 int main(int argc, char const *argv[]) {
@@ -93,9 +128,9 @@ int main(int argc, char const *argv[]) {
     while (std::getline(file, line)) {
 
       if (line.find("Query:") != std::string::npos) {
-        std::string line_q = StripLabel(line, "Query:", true);
+        std::string line_q = StripLabel(line, "Query:", false);
         if (line_q != current_query &&
-            line_q.find("|:[revcomp]") == std::string::npos) {
+            line_q.find("[revcomp]") == std::string::npos) {
           if (current_query != "")
             alignments.push_back(ai_temp);
           ai_temp = align_info();
@@ -104,7 +139,7 @@ int main(int argc, char const *argv[]) {
         }
       }
       if (line.find("Target:") != std::string::npos) {
-        std::string line_temp = StripLabel(line, "Target:", false);
+        std::string line_temp = StripLabel(line, "Target:", true);
         att_temp.d_gene = line_temp;
       }
       if (line.find("Raw score:") != std::string::npos) {
@@ -127,17 +162,42 @@ int main(int argc, char const *argv[]) {
       }
     }
   }
-  for (int q = 0; q < alignments.size(); q++) {
-    std::cout << "Query: " << alignments[q].query_name << "\n\n";
-    for (int p = 0; p < alignments[q].attributes_vec.size(); ++p) {
-      std::cout << "d gene: " << alignments[q].attributes_vec[p].d_gene << "\n";
-      std::cout << "score: " << alignments[q].attributes_vec[p].score << "\n";
-      std::cout << "Query range: " << alignments[q].attributes_vec[p].ranges[0]
-                << "-" << alignments[q].attributes_vec[p].ranges[1] << "\n";
-      std::cout << "Target range: " << alignments[q].attributes_vec[p].ranges[2]
-                << "-" << alignments[q].attributes_vec[p].ranges[3] << "\n\n";
+  // for (int q = 0; q < alignments.size(); q++) {
+  //   std::cout << "Query: " << alignments[q].query_name << "\n\n";
+  //   for (int p = 0; p < alignments[q].attributes_vec.size(); ++p) {
+  //     std::cout << "d gene: " << alignments[q].attributes_vec[p].d_gene <<
+  //     "\n";
+  //     std::cout << "score: " << alignments[q].attributes_vec[p].score <<
+  //     "\n";
+  //     std::cout << "Query range: " <<
+  //     alignments[q].attributes_vec[p].ranges[0]
+  //               << "-" << alignments[q].attributes_vec[p].ranges[1] << "\n";
+  //     std::cout << "Target range: " <<
+  //     alignments[q].attributes_vec[p].ranges[2]
+  //               << "-" << alignments[q].attributes_vec[p].ranges[3] <<
+  //               "\n\n";
+  //   }
+  // }
+
+  for (int g = 0; g < alignments.size(); g++) {
+    std::cout << "Query: " << alignments[g].query_name << "\n\n";
+    std::string dgene;
+    std::vector<attributes> vec;
+    for (int h = 0; h < alignments[g].attributes_vec.size(); ++h) {
+      if (alignments[g].attributes_vec[h].d_gene == dgene)
+        vec.push_back(alignments[g].attributes_vec[h]);
+      else {
+        if (h != 0 && vec.size() > 1) {
+          FindSecondary(vec);
+        }
+        vec.clear();
+        dgene = alignments[g].attributes_vec[h].d_gene;
+        vec.push_back(alignments[g].attributes_vec[h]);
+      }
     }
   }
+  std::cout << "Number of secondary alignments: " << n_second << "\n";
+  std::cout << "Total Aligned Queries:" << alignments.size();
   file.close();
   return 0;
 }
--- SecondaryAlignment/sec_align.sh
+++ SecondaryAlignment/sec_align.sh
@@ -1,6 +1,6 @@
 echo -e "\ns\nt\na\nr\nt\n"
 
-./ig-sw test_data/SRR1383326_collapse-unique_atleast-2.fastq test_data/useless.sam -p test_data/ -m 1 -u 1 -o 7 -e 1 -d 0
+./ig-sw test_data/SRR1383448_collapse-unique_atleast-2.fastq test_data/useless.sam -p test_data/ -m 1 -u 1 -o 7 -e 1 -d 0
 ./toletter >> truncated_seqs.fa
-exonerate --model affine:local --exhaustive yes -s 25 --showcigar true ighd.fa truncated_seqs.fa >> 25_threshold.txt
-echo 25_threshold.txt | ./parseoutput >> final_25.txt
+exonerate --model affine:local --exhaustive yes -s 50 --showcigar true truncated_seqs.fa ighd.fa >> 50_threshold.txt
+echo 50_threshold.txt | ./parseoutput >> final_file3_switched.txt
